acedeee49bd49fe869fa4087e904128eb49d04a3
MEECHAI Vincent <43035257+MEECHAIVincent@users.noreply.github.com>
2021-03-10 09:15:17 +0100

    refont du projet

--- README.md
+++
@@ -1,1 +0,0 @@
-# ProjetPhoto
---
+++ inc/jooter.php
@@ -0,0 +1,34 @@
+<footer id="footer" class="top-space">
+
+	<div class="footer2">
+		<div class="container">
+			<div class="row">
+			
+				<div class="col-md-6 widget">
+				
+			</div>
+
+			<div class="col-md-6 widget">
+				<div class="widget-body">
+					<p class="text-right">
+						Copyright &copy; 2020, Photo.   
+					</p>
+				</div>
+			</div>
+
+			</div> <!-- /row of widgets -->
+		</div>
+	</div>
+
+</footer>	
+<!-- JavaScript libs are placed at the end of the document so the pages load faster -->
+<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
+<script src="http://netdna.bootstrapcdn.com/bootstrap/3.0.0/js/bootstrap.min.js"></script>
+<script src="assets/js/headroom.min.js"></script>
+<script src="assets/js/jQuery.headroom.min.js"></script>
+<script src="assets/js/template.js"></script>
+	<!-- Google Maps -->
+	<script src="https://maps.googleapis.com/maps/api/js?key=&amp;sensor=false&amp;extension=.js"></script> 
+	<script src="assets/js/google-map.js"></script>
+	
+</html>
