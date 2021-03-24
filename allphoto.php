cfd696d0c1c180142007d1147be2380184d6bc1b
Florentxu <72868902+Florentxu@users.noreply.github.com>
2021-03-24 09:47:53 +0100

    first commit

---
+++ allphoto.php
@@ -0,0 +1,7 @@
+<?php include "inc\header.php" ?>
+<body class="allphoto">
+
+	<h1>Toutes les photos</h1>	
+</body>
+
+<?php include "inc\jooter.php" ?>
--- inc/header.php
+++ inc/header.php
@@ -16,12 +16,22 @@
 
 
 	<link rel="stylesheet" media="screen" href="http://fonts.googleapis.com/css?family=Open+Sans:300,400,700">
+	<!-- <meta name="viewport"    content="width=device-width, initial-scale=1.0">
+	<meta name="description" content="">
+	<meta name="author"      content="Sergey Pozhilov (GetTemplate.com)"> -->
+	
+	<!-- <title>LibPhoto</title> -->
+
+	<link rel="shortcut icon" href="assets/images/gt_favicon.png">
+	
 	<link rel="stylesheet" href="assets/css/bootstrap.min.css">
 	<link rel="stylesheet" href="assets/css/font-awesome.min.css">
 
 	<!-- Custom styles for our template -->
 	<link rel="stylesheet" href="assets/css/bootstrap-theme.css" media="screen" >
 	<link rel="stylesheet" href="assets/css/main.css">
+	<!-- <link rel="stylesheet" href="assets/css/style.css"> -->
+
         <script type="text/javascript" src="JQUERY/jquery-3.1.1.js"></script>
         
         
@@ -30,12 +40,15 @@
 			<div class="navbar-header">
 				<!-- Button for smallest screens -->
 				<button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse"><span class="icon-bar"></span> <span class="icon-bar"></span> <span class="icon-bar"></span> </button>
+				<a class="navbar-brand" href="index.php" style="font-family: OCR A Std, monospace">LibPhoto</a>
 			</div>
 			<div class="navbar-collapse collapse">
 				<ul class="nav navbar-nav pull-right">
 					<li><a href="index.php">Accueil</a></li>
 					<li><a href="nvarticle.php">Ajouter les Posts</a></li>
 					<li><a href="allarticles.php">Voir les Posts</a></li>
+					<li><a href="allphoto.php">Photo</a></li>
+					<!-- <li><a href="nvarticle.php">Nouvel article</a></li> -->
 					
 					<?php
 						if (isset($_SESSION['login']) && !empty($_SESSION['login']) && $_SESSION['admin']==0 ){?>
@@ -43,8 +56,7 @@
 								<li class="dropdown">
 								<a href="#" class="dropdown-toggle" data-toggle="dropdown">Mon compte<b class="caret"></b></a>
 									<ul class="dropdown-menu">
-											<li><a href="profile.php">Mes informations</a></li>
-											
+										<li><a href="profile.php">Mes informations</a></li>	
 									</ul>
 								</li>
 							    
@@ -56,29 +68,19 @@
 									<ul class="dropdown-menu">
 											<li><a href="gestionuser.php">Gestion utilisateur</a></li>
                                                                             	
+                                            <li><a href="gestionarticle.php">Gestion article</a></li>
 									</ul>
 								</li>
                                                                 <li class="dropdown">
 								<a href="#" class="dropdown-toggle" data-toggle="dropdown">Mon compte<b class="caret"></b></a>
 									<ul class="dropdown-menu">
 											<li><a href="profile.php">Mes informations</a></li>
-											
 									</ul>
 								</li>
-							    
 								<li><a class="btn" href="logout.php">Déconnexion</a></li>
-
 					<?php	} else {?>
 						<li><a class="btn" href="signin.php">Connexion / Inscription</a></li>
-							
-
 					<?php	}
-
-
-
-
-
-
 					?>
 					
 				</ul>
